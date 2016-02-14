e28b9726fcaacaeead7e608db5eec47e240a06fc
Paige Rudnick <dreambbqchild@blade-bbq.local>
2016-02-13 19:52:20 -0600

    Initial Commit

---
+++ MazeCreator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MazeCreator
+//
+//  Created by Paige Rudnick on 2/13/16.
+//  Copyright © 2016 Paige Rudnick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
